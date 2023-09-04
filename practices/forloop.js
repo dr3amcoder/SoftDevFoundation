6c3a41d82d93c8d75f15345a7fd4faa6af5ff108
dr3amcoder <jannalimpin@hotmail.co.uk>
2023-09-04 17:55:16 +0100

    Adding files from Phase 1.6

---
+++ practices/forloop.js
@@ -0,0 +1,12 @@
+// for loop code written to print numbers 0 to 10
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+};
+
+// while loop written to print numbers 0 to 10
+let i = 0;
+
+while (i <= 10) {
+    console.log(i);
+    i += 1;
+};
---
+++ practices/functionsAsValues.js
@@ -0,0 +1,16 @@
+// copy to a file called functionsAsValues.js
+// This is a function. It has no name and no parameters
+// () => {
+//   return 123;
+// }
+// This is a function with one parameter, but still no name
+// And using briefer syntax that might help when we get to .map and .filter
+// (n) => 2*n
+// you can see that it is a function like this
+// console.log((n) => 2*n)
+
+const doubleNumber = (n) => 2*n;
+console.log(doubleNumber)
+
+const simpleCalculation = doubleNumber;
+console.log(simpleCalculation);
